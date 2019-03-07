0560ed23158f77ebc623d6cca024898a02b91b8f
joshuavanderlaan <47641963+joshuavanderlaan@users.noreply.github.com>
2019-03-07 12:57:37 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+--
+-- march 7th 2019
+-- joshua van der laan
+-- displays hello world in the console
+-----------------------------------------------------------------------------------------
+
+display.setStatusBar(display.HiddenStatusBar)
+
+
+
+print( "Hello, World!")
